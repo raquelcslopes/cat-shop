008000562b0431b5320ef742e2a63e0cc1b79209
admin.mindera <raquel.lopes@school.mindera.com>
2025-07-23 16:28:47 +0100

    refactor(carousel): simplify carousel component and styles
    restructure carousel to use simpler slideshow implementation
    update styles to match new component structure
    add text display functionality to SVGButtons component

--- src/components/SVGButtons/index.tsx
+++ src/components/SVGButtons/index.tsx
@@ -1,17 +1,19 @@
 import { Image, SVGButton } from "./styles";
 
 interface SVGButtonsProps {
-    src:string,
-    alt:string,
-    onClick: () => void;
+  text?: string;
+  src: string;
+  alt: string;
+  onClick: () => void;
 }
 
-function SVGButtons({src, alt, onClick} : SVGButtonsProps)  {
+function SVGButtons({ src, alt, text, onClick }: SVGButtonsProps) {
   return (
     <SVGButton onClick={onClick}>
+      {text}
       <Image src={src} alt={alt} />
     </SVGButton>
-  )
+  );
 }
 
-export default SVGButtons
+export default SVGButtons;
--- src/pages/BreedPage/index.tsx
+++ src/pages/BreedPage/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import arrowsIcon from "../../assets/arrows.svg"
+import SVGButtons from '../../components/SVGButtons'
 
 function BreedsPage() {
   return (
     <>
-    <div></div>
+    <div><SVGButtons src={arrowsIcon} alt={'Arrows to order'} onClick={function (): void {
+        throw new Error('Function not implemented.')
+      } } text="Price"></SVGButtons>
+      
+      <SVGButtons src={arrowsIcon} alt={'Arrows to order'} onClick={function (): void {
+        throw new Error('Function not implemented.')
+      } } text="AZ"></SVGButtons></div>
     <div></div>
     </>
   )
